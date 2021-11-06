b7b665add2dd08e4b78d1a651adc08d46ea06f8c
LukacTomas <lukactv@gmail.com>
2021-11-06 05:45:18 +0100

    index added to Theme/Components so it export all the components

---
+++ src/Theme/Components/index.js
@@ -0,0 +1,6 @@
+import { Background } from "./Background";
+import { Mouseeffect } from "./Mouseeffect";
+import { Particles } from "./Particles";
+import { SayHi } from "./Sayhi";
+
+export { Background, Mouseeffect, Particles, SayHi };
