f26c3dc04b742678b98e7b39d3893fc2a992109f
LukacTomas <lukactv@gmail.com>
2021-11-06 21:29:19 +0100

    Routes refactored to a Directory

---
+++ src/Theme/Components/Routes/Transitionroutes.js
@@ -0,0 +1,32 @@
+import React, { Suspense } from "react";
+import { Routes as Switch, Route, Navigate } from "react-router-dom";
+
+const Resume = React.lazy(() => import("Pages/Resume"));
+
+export const Transitionroutes = ({ children }) => {
+  return (
+    <Switch>
+      <Route exact={true} path="en/home/" element={<h1>EN HOME</h1>} />
+      <Route
+        exact={true}
+        path="en/resume/"
+        element={
+          <Suspense fallback={<></>}>
+            <Resume />
+          </Suspense>
+        }
+      />
+      <Route exact={true} path="sk/home/" element={<h1>SK HOME</h1>} />
+      <Route
+        exact={true}
+        path="sk/resume/"
+        element={
+          <Suspense fallback={<></>}>
+            <Resume />
+          </Suspense>
+        }
+      />
+      <Route path="" element={<Navigate to="/en/home/" />} />
+    </Switch>
+  );
+};
