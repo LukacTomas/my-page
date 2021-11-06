8140f56190fe2a6623812be3d4eaf777281f797e
LukacTomas <lukactv@gmail.com>
2021-11-06 06:52:07 +0100

    Config dir added to ./src with index file ./src/Config/index.js with path export

---
+++ src/Config/index.js
@@ -0,0 +1,14 @@
+export const paths = {
+  en: {
+    Home: { route: "/en/home/", icon: "🏠", id: "Home" },
+    //Blog: { route: "/en/blog/", icon: "📚", id: "Blog" },
+    //Course: { route: "/en/course/", icon: "🎓", id: "Course" },
+    Resume: { route: "/en/resume", icon: "📋", id: "Resume" },
+  },
+  sk: {
+    Domov: { route: "/sk/home/", icon: "🏠", id: "Home" },
+    //Blog: { route: "/sk/blog/", icon: "📚", id: "Blog" },
+    //Kurz: { route: "/sk/course/", icon: "🎓", id: "Course" },
+    Resumé: { route: "/sk/resume", icon: "📋", id: "Resume" },
+  },
+};
