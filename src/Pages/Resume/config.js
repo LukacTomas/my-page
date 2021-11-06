1b5ea8ccf57d285faea52ae1a00df4b3db8d0d6a
LukacTomas <lukactv@gmail.com>
2021-11-06 18:40:40 +0100

    Grid set up in Resume

--- src/Pages/Resume/config.js
+++ src/Pages/Resume/config.js
@@ -1,103 +1,109 @@
-export const skData = {
-  helmet: {
-    title: "Lukac Tomas | Resumé",
-    description:
-      "Tomas Lukac, resumé, cv, programovanie, React, Git a co ešte?",
+const data = {
+  sk: {
+    helmet: {
+      title: "Lukac Tomas | Resumé",
+      description:
+        "Tomas Lukac, resumé, cv, programovanie, React, Git a co ešte?",
+    },
+    title: "🚀Tomáš Lukáč",
+    jobs: [
+      {
+        title: "Učiteľ",
+        date: {
+          from: 2012,
+          to: "súčasnosť",
+        },
+        description:
+          "Učiteľ na čiastočný úväzok pre odborné predmety ako elektronika, programovanie, pneumatika, hydraulika. Plný úväzok od septembra 2020",
+        supervisor: {
+          name: "SPŠ strojnícka",
+          location: "Bratislava",
+        },
+      },
+      {
+        title: "Lektor / Tréner / Poradca",
+        date: {
+          from: 2013,
+          to: 2020,
+        },
+        description:
+          "Lektor a tréner a poradca pre kurzy ako pneumatika a elektropneumatika, hydraulika, elektronika, frekvenčné meniče a pod...",
+        supervisor: {
+          name: "Festo",
+          location: "Bratislava",
+        },
+      },
+    ],
+    school: [
+      {
+        title: "Elektronika a rádioelektronika",
+        date: {
+          from: 2007,
+          to: 2012,
+        },
+        description:
+          "Bakalársky program elektronika na fakulte elektrotechnika a informatiky. Inžiniersky program rádioelektronika",
+        supervisor: {
+          name: "Slovenská technická univezita",
+          location: "Bratislava",
+        },
+      },
+    ],
   },
-  title: "🚀Tomáš Lukáč",
-  jobs: [
-    {
-      title: "Učiteľ",
-      date: {
-        from: 2012,
-        to: "súčasnosť",
-      },
+
+  en: {
+    helmet: {
+      title: "Lukac Tomas | Resume",
       description:
-        "Učiteľ na čiastočný úväzok pre odborné predmety ako elektronika, programovanie, pneumatika, hydraulika. Plný úväzok od septembra 2020",
-      supervisor: {
-        name: "SPŠ strojnícka",
-        location: "Bratislava",
-      },
+        "Tomas Lukac, resume, CV, programming, React, Git and what else?",
     },
-    {
-      title: "Lektor / Tréner / Poradca",
-      date: {
-        from: 2013,
-        to: 2020,
+    title: "Tomas Lukac",
+    jobs: [
+      {
+        title: "Teacher",
+        date: {
+          from: 2012,
+          to: "present",
+        },
+        description:
+          "Part-time teacher of vocational subject i.e.: electronics, programming, automation, mechatronics. Full time-teacher from 2020",
+        supervisor: {
+          name: "SPŠ strojnícka",
+          location: "Bratislava",
+        },
       },
-      description:
-        "Lektor a tréner a poradca pre kurzy ako pneumatika a elektropneumatika, hydraulika, elektronika, frekvenčné meniče a pod...",
-      supervisor: {
-        name: "Festo",
-        location: "Bratislava",
+      {
+        title: "Lector / Trainer / Advaisor",
+        date: {
+          from: 2013,
+          to: 2020,
+        },
+        description:
+          "Lecturer / trainer/ advisor for courses pneumatics, hydraulics, electrotechnics, frequency converters etc.",
+        supervisor: {
+          name: "Festo",
+          location: "Bratislava",
+        },
       },
-    },
-  ],
-  school: [
-    {
-      title: "Elektronika a rádioelektronika",
-      date: {
-        from: 2007,
-        to: 2012,
+    ],
+    school: [
+      {
+        title: "Elektronics and radioelektronics",
+        date: {
+          from: 2007,
+          to: 2012,
+        },
+        description:
+          "Bachelor degree in electronics at Faculty of Electrical Engineering. Master’s degree in radio-electronics at Faculty of Electrical Engineering",
+        supervisor: {
+          name: "Slovak Univesity of Technology",
+          location: "Bratislava",
+        },
       },
-      description:
-        "Bakalársky program elektronika na fakulte elektrotechnika a informatiky. Inžiniersky program rádioelektronika",
-      supervisor: {
-        name: "Slovenská technická univezita",
-        location: "Bratislava",
-      },
-    },
-  ],
+    ],
+  },
 };
 
-export const enData = {
-  helmet: {
-    title: "Lukac Tomas | Resume",
-    description:
-      "Tomas Lukac, resume, CV, programming, React, Git and what else?",
-  },
-  title: "Tomas Lukac",
-  jobs: [
-    {
-      title: "Teacher",
-      date: {
-        from: 2012,
-        to: "present",
-      },
-      description:
-        "Part-time teacher of vocational subject i.e.: electronics, programming, automation, mechatronics. Full time-teacher from 2020",
-      supervisor: {
-        name: "SPŠ strojnícka",
-        location: "Bratislava",
-      },
-    },
-    {
-      title: "Lector / Trainer / Advaisor",
-      date: {
-        from: 2013,
-        to: 2020,
-      },
-      description:
-        "Lecturer / trainer/ advisor for courses pneumatics, hydraulics, electrotechnics, frequency converters etc.",
-      supervisor: {
-        name: "Festo",
-        location: "Bratislava",
-      },
-    },
-  ],
-  school: [
-    {
-      title: "Elektronics and radioelektronics",
-      date: {
-        from: 2007,
-        to: 2012,
-      },
-      description:
-        "Bachelor degree in electronics at Faculty of Electrical Engineering. Master’s degree in radio-electronics at Faculty of Electrical Engineering",
-      supervisor: {
-        name: "Slovak Univesity of Technology",
-        location: "Bratislava",
-      },
-    },
-  ],
+export const useData = (lang) => {
+  return data[lang];
 };
--- src/Pages/Resume/index.js
+++ src/Pages/Resume/index.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Grid } from "@mui/material";
 import { useLanguage } from "Hooks";
+import { useData } from "./config";
 
-const Resume =  () => {
+const Resume = () => {
   const lang = useLanguage();
 
-  return <div>{lang}</div>;
+  // load data based on current language
+  const data = useData(lang);
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={2}
+        direction="row-reverse"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          LEFT
+        </Grid>
+        <Grid item xs={12} md={6} lg={8}>
+          RIGHT
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Resume;
