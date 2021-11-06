3ead0c5204a98656ad126d94541527fd058bad05
LukacTomas <lukactv@gmail.com>
2021-11-06 14:35:07 +0100

    Hook added to detect screen change, files for LinksMobile, LinksScreen created

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -1,3 +1,6 @@
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 export const useLanguage = () => {
   const location = window.location.pathname;
   let language = location.split("/").filter((l) => l !== "");
@@ -13,10 +16,22 @@ export const useLanguage = () => {
 export const useCurrentPage = () => {
   const location = window.location.pathname;
   let page = location.split("/").filter((l) => l !== "");
-   if (page.length > 1) {
-     page = page[1];
-   } else {
-     page = "/";
-   }
+  if (page.length > 1) {
+    page = page[1];
+  } else {
+    page = "/";
+  }
   return page;
+};
+
+export const useWidth = () => {
+  const theme = useTheme();
+  const keys = [...theme.breakpoints.keys].reverse();
+  return (
+    keys.reduce((output, key) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const matches = useMediaQuery(theme.breakpoints.up(key));
+      return !output && matches ? key : output;
+    }, null) || "xs"
+  );
 };
--- src/Theme/Components/Navigation/Menu/index.js
+++ src/Theme/Components/Navigation/Menu/index.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { useWidth } from "Hooks";
 
 export const Menu = () => {
+  const width = useWidth();
+  if (width === "xs" || width === "sm") return <div>Menu</div>;
+
   return <div>My new Menu</div>;
 };
