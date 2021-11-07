1ca1f8cb138f740ab2cd61603020d4dfdecfeb9d
LukacTomas <lukactv@gmail.com>
2021-11-07 08:06:58 +0100

    Background improved so moon is visible

--- src/Theme/Components/Background.js
+++ src/Theme/Components/Background.js
@@ -7,13 +7,15 @@ const BackgroundDiv = styled("div")(({ theme }) => ({
   height: "100%",
   top: 0,
   //left: "50vw",
+  clipPath:
+    "polygon(0% 0%, 100% 0%, 100% 100%, 40% 23%, 52% 31%, 63% 43%, 69% 56%, 77% 68%, 86% 80%)",
   backgroundColor: theme.palette.background.backgroundDiv.first,
-
+  //backgroundColor: "red",
   [theme.breakpoints.up("xs")]: {
-    background: `linear-gradient(10deg, ${theme.palette.background.backgroundDiv.first} 0%,
-      ${theme.palette.background.backgroundDiv.first} 20%, 
-      ${theme.palette.background.backgroundDiv.second} 80%,
-      ${theme.palette.background.backgroundDiv.third} 96%)`,
+    background: `linear-gradient(32deg, ${theme.palette.background.backgroundDiv.first} 0%,
+      ${theme.palette.background.backgroundDiv.first} 60%, 
+      ${theme.palette.background.backgroundDiv.second} 70%,
+      ${theme.palette.background.backgroundDiv.third} 100%)`,
   },
   zIndex: -1,
 }));
