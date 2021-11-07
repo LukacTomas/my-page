6e2c0c6316660c783da0e96e40417c1ccd150c7e
LukacTomas <lukactv@gmail.com>
2021-11-07 06:00:32 +0100

    Simplet Transition between pages but first render flickers

--- src/Theme/Components/Routes/Transitionroutes.js
+++ src/Theme/Components/Routes/Transitionroutes.js
@@ -1,32 +1,49 @@
 import React, { Suspense } from "react";
-import { Routes as Switch, Route, Navigate } from "react-router-dom";
+import {
+  Routes as Switch,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import "./Transitionroutes.css";
 
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 const Resume = React.lazy(() => import("Pages/Resume"));
 
 export const Transitionroutes = ({ children }) => {
+  let location = useLocation();
+
   return (
-    <Switch>
-      <Route exact={true} path="en/home/" element={<h1>EN HOME</h1>} />
-      <Route
-        exact={true}
-        path="en/resume/"
-        element={
-          <Suspense fallback={<></>}>
-            <Resume />
-          </Suspense>
-        }
-      />
-      <Route exact={true} path="sk/home/" element={<h1>SK HOME</h1>} />
-      <Route
-        exact={true}
-        path="sk/resume/"
-        element={
-          <Suspense fallback={<></>}>
-            <Resume />
-          </Suspense>
-        }
-      />
-      <Route path="" element={<Navigate to="/en/home/" />} />
-    </Switch>
+    <TransitionGroup>
+      <CSSTransition
+        key={location.pathname}
+        timeout={{ enter: 750, exit: 0 }}
+        classNames="page"
+      >
+        <Switch location={location}>
+          <Route exact={true} path="en/home/" element={<h1>EN HOME</h1>} />
+          <Route
+            exact={true}
+            path="en/resume/"
+            element={
+              <Suspense fallback={<></>}>
+                <Resume />
+              </Suspense>
+            }
+          />
+          <Route exact={true} path="sk/home/" element={<h1>SK HOME</h1>} />
+          <Route
+            exact={true}
+            path="sk/resume/"
+            element={
+              <Suspense fallback={<></>}>
+                <Resume />
+              </Suspense>
+            }
+          />
+          <Route path="" element={<Navigate to="/en/home/" />} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
